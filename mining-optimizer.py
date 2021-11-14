7fce2835ef1209b269d4ef40e7082594502f60fd
Tero Partanen <teropar@gmail.com>
2021-11-14 22:58:39 +0200

    fixed final report, the best efficiency reported wrong hashrate

--- mining-optimizer.py
+++ mining-optimizer.py
@@ -24,13 +24,13 @@ import time
 ### SETTINGS
 miner = 0 # t-rex = 0, phoenixminer = 1
 gpu = 0 #which GPU we are testing 0,1,2,3, or .. (only one)
-power_limits = [160,160] #GPU power limits in testing, [low,high]. Testing each power setting from low to high with defined steps. NOTE, if using absolute core clock this might be better to use just as upper limits (low=high)
+power_limits = [150,160] #GPU power limits in testing, [low,high]. Testing each power setting from low to high with defined steps. NOTE, if using absolute core clock this might be better to use just as upper limits (low=high)
 power_step = 10 #
-gpu_core_limits = [600, 1500] #core clock [low,hig] offset limits. If value is 500 or less, it is considered as offset, otherwise it is absolute value
+gpu_core_limits = [1300, 1500] #core clock [low,hig] offset limits. If value is 500 or less, it is considered as offset, otherwise it is absolute value
 core_step = 25 #core offset to increase in each step
 gpu_mem_limits = [2100, 2400] #memory clock offset limits
 mem_step = 100 #memory clock to increase in each step
-step_time = 120 #how many seconds we run each setting
+step_time = 60 #how many seconds we run each setting
 result_divider = 1000000 #1000 to produce KH/s or 1000000 for MH/s
 save_file = True #write results to a file: results.log
 ###
@@ -56,6 +56,7 @@ def get_hash_pow(miner, gpu,time_step):
         for i in range(4): 
             time.sleep(time_step/4)
             pow_temp = query_gpu(gpu,"power.draw")
+            print(pow_temp)  #DEBUG
             if(pow_temp > 0):
                 pow_sum = pow_sum + pow_temp
                 divider = divider+1
@@ -162,7 +163,9 @@ mem_values = range(gpu_mem_limits[0],gpu_mem_limits[1]+mem_step,mem_step)
 results_log = [] #store all results in this, currently no used
 #open the file   
 if(save_file):
-    filename = "./results_gpu" + str(gpu) + "_p" + str(power_values[0]) + "-" + str(power_values[-1]) + "_c" + str(core_values[0]) + "-" + str(core_values[-1]) + "_m" + str(mem_values[0]) + "-" + str(mem_values[-1]) + ".log"
+    localtime = time.localtime(time.time()) #use time and date in filename. h:m:s-d.m.y
+    timestring = str(localtime[3]) + ":" + str(localtime[4]) + ":" + str(localtime[5]) + "-" + str(localtime[2]) + "." + str(localtime[1]) + "." + str(localtime[0])
+    filename = "./results_" + str(timestring) + "_miner" + str(miner) + "_gpu" + str(gpu) + "_p" + str(power_values[0]) + "-" + str(power_values[-1]) + "_c" + str(core_values[0]) + "-" + str(core_values[-1]) + "_m" + str(mem_values[0]) + "-" + str(mem_values[-1]) + ".log"
     with open(filename, 'w') as the_file:
         the_file.write("Hashrate\treported W\tcore\tmem\tefficiency (hashrate/W)\n")
     
@@ -188,7 +191,7 @@ if(output1.returncode == 4):
         print("Only memory values are changed, testing the memory values from min to max")
 output2 = set_core_clk(gpu,gpu_core_limits[0],False)
 #set the lowest memory clock
-output3 = set_mem_clk(gpu,gpu_mem_limits[0],True)
+output3 = set_mem_clk(gpu,gpu_mem_limits[0],False)
 
 if(output2.returncode == 0 and output3.returncode == 0):
     print("\nInitial settings successfull, testing will start..."); print("Testing iteratively all the combinations:")
@@ -228,6 +231,7 @@ if(output2.returncode == 0 and output3.returncode == 0):
                     best_settings = (power,core,mem, efficiency) #efficiency not really a setting, but save it here also
                 if(efficiency > best_efficiency):
                     best_efficiency = efficiency
+                    best_eff_rate = hashrate
                     best_eff_settings = (power,core,mem, efficiency)
                 print("Hashrate: " + str(hashrate) + ", efficiency: " + str(efficiency) + "  (best: " + str(best_rate) + ", with " + str(best_settings[0]) + "/" + str(best_settings[1]) + "/" + str(best_settings[2]) + ", eff: " + str(best_settings[3]) + ")")
                 #save the results
@@ -246,7 +250,7 @@ if(output2.returncode == 0 and output3.returncode == 0):
         print("Writing results to file " + filename)
         with open(filename, 'a') as the_file:
             the_file.write("Best hash rate settings, power: " + str(best_settings[0]) + ", core: " + str(best_settings[1]) + ", mem: " + str(best_settings[2]) + ", hashrate: " + str(best_rate) + "  efficiency: " + str(best_settings[3]) + "\n")
-            the_file.write("Best efficiency settings, power: " + str(best_eff_settings[0]) + ", core: " + str(best_eff_settings[1]) + ", mem: " + str(best_eff_settings[2]) + ", hashrate: " + str(best_rate) + "  efficiency: " + str(best_eff_settings[3]) + "\n")
+            the_file.write("Best efficiency settings, power: " + str(best_eff_settings[0]) + ", core: " + str(best_eff_settings[1]) + ", mem: " + str(best_eff_settings[2]) + ", hashrate: " + str(best_eff_rate) + "  efficiency: " + str(best_eff_settings[3]) + "\n")
     #set the best core      
     set_core_clk(gpu,best_settings[1],True)
     #and set the best memory clock
